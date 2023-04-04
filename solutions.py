d16fe5c934835f914928faf3782cde40dfc3ad68
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-04-04 15:17:29 +0530

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -3,7 +3,10 @@
 
 ANSWER..
 
-Both def statements and lambda expressions are used to define functions in Python. However, def statements are used to define named functions with multiple statements and blocks of code, while lambda expressions are used to define anonymous functions that can be written in a single line.
+Both def statements and lambda expressions are used to define functions in Python.
+However, def statements are used to define named functions with multiple statements
+and blocks of code, while lambda expressions are used to define anonymous functions
+that can be written in a single line.
 
 ---------------------------------------------------------------------------------------------------
 
@@ -21,7 +24,12 @@ The benefit of lambda is that it allows for the creation of small, one-line func
 
 ANSWER..
 
-map, filter, and reduce are all functions used in functional programming. map applies a given function to every element in a list and returns a new list with the results. filter applies a given function to every element in a list and returns a new list with only the elements that return True. reduce applies a given function to the first two elements in a list, then to the result and the next element, and so on, until all elements have been processed.
+map, filter, and reduce are all functions used in functional programming. map applies
+a given function to every element in a list and returns a new list with the results.
+filter applies a given function to every element in a list and returns a new list with
+only the elements that return True. reduce applies a given function to the first two
+elements in a list, then to the result and the next element, and so on, until all elements
+have been processed.
 
 
 ---------------------------------------------------------------------------------------------------
@@ -31,7 +39,9 @@ map, filter, and reduce are all functions used in functional programming. map ap
 
 ANSWER..
 
-Function annotations are a feature introduced in Python 3 that allow you to add metadata to function arguments and return values. They are used to provide more information about what a function does and what data types it expects.
+Function annotations are a feature introduced in Python 3 that allow you to add metadata
+to function arguments and return values. They are used to provide more information about
+what a function does and what data types it expects.
 
 
 ---------------------------------------------------------------------------------------------------
@@ -41,7 +51,9 @@ Function annotations are a feature introduced in Python 3 that allow you to add 
 
 ANSWER..
 
-Recursive functions are functions that call themselves in order to solve a problem. They are used to solve problems that can be broken down into smaller and smaller sub-problems, until the base case is reached.
+Recursive functions are functions that call themselves in order to solve a problem.
+They are used to solve problems that can be broken down into smaller and smaller
+sub-problems, until the base case is reached.
 
 
 ---------------------------------------------------------------------------------------------------
@@ -51,7 +63,9 @@ Recursive functions are functions that call themselves in order to solve a probl
 
 ANSWER..
 
-Some general design guidelines for coding functions include: keeping functions small and focused on a single task, using descriptive and meaningful names, using function arguments instead of global variables, and avoiding side effects.
+Some general design guidelines for coding functions include: keeping functions small
+and focused on a single task, using descriptive and meaningful names, using function
+arguments instead of global variables, and avoiding side effects.
 
 
 ---------------------------------------------------------------------------------------------------
@@ -61,7 +75,9 @@ Some general design guidelines for coding functions include: keeping functions s
 
 ANSWER..
 
-Three ways that functions can communicate results to a caller are: using return statements to return a value or values, using print statements to output values to the console, and modifying mutable objects that are passed in as arguments.
+Three ways that functions can communicate results to a caller are: using return statements
+to return a value or values, using print statements to output values to the console, and
+modifying mutable objects that are passed in as arguments.
 
 
 ---------------------------------------------------------------------------------------------------
